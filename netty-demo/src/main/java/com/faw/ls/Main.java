aa5b57aa19b7378136129281fd069973594eb58f
李帅 <ls01622496@alibaba-inc.com>
2023-02-23 20:58:19 +0800

    bufferTest

---
+++ netty-demo/src/main/java/com/faw/ls/Main.java
@@ -0,0 +1,11 @@
+package com.faw.ls;
+
+/**
+ * @author ${USER}
+ * @date ${YEAR}/${MONTH}/${DAY}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
